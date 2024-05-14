3d8f5811a4c34df0c59ce7ea0eca8968b5a9c965
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-05-14 16:44:01 -0600

    si

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short cantidad  = 0 ;
+        double precio = 0;
+        double total = 0;
+
+
+        //pedir la cantidad 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de productos a comprar: ");
+        cantidad = sc.nextShort();
+
+        //pedir el precio
+        System.out.print("Ingrese el precio del producto: ");
+        precio = sc.nextDouble();
+        sc.close();
+
+        //calcular el total
+        total = cantidad * precio;
+        
+        //imprimir el total
+         System.out.println("el total es : " + total);
+
     }
 }
